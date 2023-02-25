91fbfa665034db3a510ee94d3d2ec5fe4eef3d18
Fabricio Madruga <madruga.fabricio@hotmail.com>
2023-02-24 22:20:21 -0300

    fixture

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import styles from './styles';
 
-interface ICard {
+export interface ICard {
   title: string;
   children: React.ReactNode;
 }
